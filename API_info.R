438a09364b2f85bc139e7e43ca48696e7a53ca9b
longhow lam <longhowlam@gmail.com>
2018-07-23 16:11:48 +0200

    Add files via upload

--- API_info.R
+++ API_info.R
@@ -2,6 +2,7 @@ library(httr)
 library(rvest)
 library(purrr)
 library(tibble)
+library(dplyr)
 
 mykey = "fY3aMpfVj2FE8nTEVQWPoP09Xs7dngEVvrLZKUh2"
 
@@ -26,17 +27,38 @@ searchout  = GET(
 mcdonalds = map_df(searchout[["list"]][["item"]], as.tibble)
 
 
-############################ food nutrient report ##############################
+############################ food nutrient report for one ndbno ##############################
+ndbno_in = 21238
+getnutr_values = function(ndbno_in)
+{
+  nutrientsURL =  "https://api.nal.usda.gov/ndb/reports/"
 
-nutrientsURL =  "https://api.nal.usda.gov/ndb/reports/"
+  ntr = GET(
+    url = nutrientsURL,
+    query = list(
+      api_key = mykey ,
+      format = "json",
+      ndbno = ndbno_in
+    )
+  ) %>%  content()
 
-resp2 = GET(
-  url = nutrientsURL,
-  query = list(
-    api_key = mykey ,
-    format = "json",
-    ndbno = "01009"
-  )
-)
+  nutrientdata = function(x)
+  {
+    nutrdata = map_df(x$nutrients, function(x){x[1:6] %>% as.tibble})
+    nutrdata$ndbno = x$ndbno
+    nutrdata$naam = x$name
+    nutrdata %>% select(naam,ndbno,name,group, unit, value)
+  }
 
-ntr = content(resp2)
+  nutrientdata(x=ntr[["report"]][["food"]])
+}
+
+getnutr_values(21238)
+
+
+####  get values for all mcdonalds products
+
+mcdonalds_nutr = map_df( mcdonalds$ndbno, getnutr_values) %>%  distinct()
+
+
+mcdondalds_nutr %>% group_by(ndbno) %>%  summarise(n=n())
