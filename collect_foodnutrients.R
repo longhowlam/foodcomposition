fe6fad8e1bf7657e4a15334fe832964eceacdf7f
longhowlam <longhowlam@gmail.com>
2018-07-24 11:14:24 +0200

    extra foods

--- collect_foodnutrients.R
+++ collect_foodnutrients.R
@@ -12,8 +12,19 @@ Veggies = searchFood(fg = "Vegetables and Vegetable Products") %>%
 Fruits = searchFood(fg = "Fruits and Fruit Juices") %>% 
   transpose_nutrients()
 
+Fish = searchFood(fg = "Finfish and Shellfish Products") %>% 
+  transpose_nutrients()
+
+Pork = searchFood(fg = "Pork Products") %>% 
+  transpose_nutrients()
+
+Beef = searchFood(fg = "Beef Products") %>% 
+  transpose_nutrients()
+
+Poultry = searchFood(fg = "Poultry Products") %>% 
+  transpose_nutrients()
 
 food = bind_rows(
-  mcdonalds, pizzahut, apple, broccoli, tomatoes, banana, Veggies, Fruits
+  mcdonalds, pizzahut, Veggies, Fruits, Fish, Pork, Beef, Poultry
 ) %>% 
   mutate_if(is.numeric, function(x){ifelse(is.na(x),0,x)})
